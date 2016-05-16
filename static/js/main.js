f87a082b75dd47dfdf0a4ad7569626e37fefcd5b
napster <mitchell-mays>
2016-05-16 15:29:06 -0400

    help bradley

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,7 @@
   var FaceDetect = function(webcam, canvas) {
     var self = this;
 
+
     if (!self.hasGetUserMedia()) {
       throw("getUserMedia() is not supported in your browser");
     }
@@ -18,6 +19,8 @@
     self.canvas = canvas;
 
     self.maxWorkSize = 160;
+    self.currRect = null;
+    self.rectChangeThresh = 4;
 
     //Init fallback functions
     navigator.FaceDetectUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
@@ -103,9 +106,9 @@
 
     if (self.webcam.readyState === self.webcam.HAVE_ENOUGH_DATA) {
       var rects = [];
-      var angles = [0, -1, -0.5, 0.5, 1];
-      //self.ctx.drawImage(self.webcam, 0, 0, self.canvas.width, self.canvas.height);
-      self.ctx.drawImage(self.webcam, 0, 0, self.workCanvas.width, self.workCanvas.height);
+      var angles = [0, -0.5, 0.5];
+      self.ctx.drawImage(self.webcam, 0, 0, self.canvas.width, self.canvas.height);
+      //self.ctx.drawImage(self.webcam, 0, 0, self.workCanvas.width, self.workCanvas.height);
 
       var x = self.workCanvas.width / 2;
       var y = self.workCanvas.height / 2;
@@ -135,18 +138,26 @@
 
         
         for (var currentRect = 0; currentRect < tempRects.length; currentRect++){
-            //tempRects[currentRect].x = tempRects[currentRect].x-x;
-            //tempRects[currentRect].y = tempRects[currentRect].y-y;
+            tempRects[currentRect].x = tempRects[currentRect].x-x;
+            tempRects[currentRect].y = tempRects[currentRect].y-y;
             //console.log(tempRects[currentRect].x);
             //console.log(tempRects[currentRect].y);
 
-            //var r = Math.sqrt(Math.pow(tempRects[currentRect].x,2) + Math.pow(tempRects[currentRect].y,2));
+            var r = Math.sqrt(Math.pow(tempRects[currentRect].x,2) + Math.pow(tempRects[currentRect].y,2));
+            var o = Math.atan(tempRects[currentRect].y/tempRects[currentRect].x);
+
+            if (tempRects[currentRect].x < 0){
+              o = o - Math.PI;
+            }
 
             //console.log(tempRects[currentRect].x + r * Math.cos(angles[i]));
             //console.log(tempRects[currentRect].y + r * Math.sin(angles[i]));
 
-            //tempRects[currentRect].x = tempRects[currentRect].x + r * Math.cos(angles[i]);
-            //tempRects[currentRect].y = tempRects[currentRect].y + r * Math.sin(angles[i]);
+            //console.log(tempRects[currentRect].x);
+            //console.log(tempRects[currentRect].y);
+
+            tempRects[currentRect].x = (r * Math.cos(o - angles[i]));
+            tempRects[currentRect].y = (r * Math.sin(o - angles[i]));
 
             //console.log(tempRects[currentRect].x);
             //console.log(tempRects[currentRect].y);
@@ -176,22 +187,54 @@
     n = Math.min(n, on);
     var r;
 
-    var x = self.workCanvas.width / 2;
-    var y = self.workCanvas.height / 2;
-    
+    //var x = self.workCanvas.width / 2;
+    //var y = self.workCanvas.height / 2;
+    var x = self.canvas.width / 2;
+    var y = self.canvas.height / 2;
+
     for(var i = 0; i < n; ++i) {
-        r = rects[i];
+          r = rects[i];
 
-        self.ctx.translate(x, y);
-        self.ctx.rotate(r.angle);
-        //self.ctx.drawImage(self.webcam, -self.workCanvas.width / 2, -self.workCanvas.height / 2, self.workCanvas.width, self.workCanvas.height);
-        self.ctx.strokeRect((r.x) | 0, (r.y) | 0, (r.width) | 0, (r.height) | 0);
-        //self.ctx.strokeRect((r.x*sc) | 0, (r.y*sc) | 0, (r.width*sc) | 0, (r.height*sc) | 0);
-        self.ctx.rotate(-r.angle)
-        self.ctx.translate(-x, -y);
+          if (self.currRect == null){
+            self.currRect = r;
+          }
+          var totDiff = Math.abs(self.currRect.x - r.x) + Math.abs(self.currRect.x - r.x) + Math.abs(self.currRect.width - r.width) + Math.abs(self.currRect.height - r.height);
+          if (totDiff < self.rectChangeThresh){
+            r = self.currRect;
+          }
+          else{
+            self.currRect = r;
+          }
 
+
+
+          self.currRect = r;
+
+          self.ctx.translate(x, y);
+          //self.ctx.drawImage(self.webcam, -self.workCanvas.width / 2, -self.workCanvas.height / 2, self.workCanvas.width, self.workCanvas.height);
+          self.ctx.rotate(-r.angle);
+          //self.ctx.drawImage(self.webcam, -self.workCanvas.width / 2, -self.workCanvas.height / 2, self.workCanvas.width, self.workCanvas.height);
+          var yChange = 0;
+          var xChange = 0;
+          if (r.angle < 0) {
+            xChange = -1 * (r.width / 2);
+          }
+          else if (r.angle > 0){
+            yChange = -1 * (r.height / 2);
+            xChange = (r.width / 4);
+          }
+
+          //self.ctx.strokeRect((r.x + xChange) | 0, (r.y + yChange) | 0, (r.width) | 0, (r.height) | 0);
+
+          //self.ctx.strokeRect(0, 0, self.workCanvas.width, self.workCanvas.height);
+          self.ctx.strokeRect(((r.x + xChange) * sc) | 0, ((r.y + yChange) * sc) | 0, (r.width * sc) | 0, (r.height * sc) | 0);
+          //console.log(r.x * sc);
+          //console.log(r.y * sc);
+          self.ctx.rotate(r.angle);
+          self.ctx.translate(-x, -y);
         
     }
+
 
   };
 
